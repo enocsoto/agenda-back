51b46b390b582665ab88abd6e587c8d492d50f00
fancisco Jimenez <devfran08@gmail.com>
2023-05-21 16:48:13 -0300

    se agrega dto para creacion de contactos

--- src/contact/dto/create-contact.dto.ts
+++ src/contact/dto/create-contact.dto.ts
@@ -1,1 +1,98 @@
-export class CreateContactDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsString,
+  IsNotEmpty,
+  IsEmail,
+  IsEnum,
+  IsDateString,
+  ArrayNotEmpty,
+  IsArray,
+} from 'class-validator';
+
+enum DocumentType {
+  ID_CARD = 'id_card',
+  PASSPORT = 'passport',
+  DRIVER_LICENSE = 'driver_license',
+}
+
+enum PhoneType {
+  LANDLINE = 'landline',
+  MOBILE = 'mobile',
+}
+
+class AddressDto {
+  @ApiProperty({ example: '123 Main St', description: 'Street address' })
+  @IsString()
+  @IsNotEmpty()
+  address: string;
+
+  @ApiProperty({ example: 'Cityville', description: 'City' })
+  @IsString()
+  @IsNotEmpty()
+  city: string;
+}
+
+class PhoneDto {
+  @ApiProperty({ example: '123456789', description: 'Phone number' })
+  @IsString()
+  @IsNotEmpty()
+  phoneNumber: string;
+
+  @ApiProperty({
+    example: 'landline',
+    enum: PhoneType,
+    description: 'Phone type',
+  })
+  @IsEnum(PhoneType)
+  @IsNotEmpty()
+  phoneType: PhoneType;
+}
+
+export class CreateContactDto {
+  @ApiProperty({ example: 'John', description: 'First name' })
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @ApiProperty({ example: 'Doe', description: 'Last name' })
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+
+  @ApiProperty({
+    example: 'id_card',
+    enum: DocumentType,
+    description: 'Document type',
+  })
+  @IsEnum(DocumentType)
+  @IsNotEmpty()
+  documentType: DocumentType;
+
+  @ApiProperty({ example: '123456789', description: 'Document number' })
+  @IsString()
+  @IsNotEmpty()
+  documentNumber: string;
+
+  @ApiProperty({ example: '1990-01-01', description: 'Date of birth' })
+  @IsDateString()
+  @IsNotEmpty()
+  birthDate: string;
+
+  @ApiProperty({
+    example: 'john.doe@example.com',
+    description: 'Email address',
+  })
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @ApiProperty({ type: [AddressDto], description: 'Addresses' })
+  @IsArray()
+  @ArrayNotEmpty()
+  addresses: AddressDto[];
+
+  @ApiProperty({ type: [PhoneDto], description: 'Phone numbers' })
+  @IsArray()
+  @ArrayNotEmpty()
+  phones: PhoneDto[];
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Configurar opciones del documento Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Agenda BackEnd API')
+    .setDescription(
+      'The Agenda API provides functionality to manage contacts, addresses, and phone numbers in an address book. It allows performing CRUD (Create, Read, Update, Delete) operations on the different entities.',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  const configservice = app.get(ConfigService);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(configservice.get('PORT'), () => {
+    Logger.log(
+      'Service listening on port : ' + configservice.get('PORT'),
+      bootstrap.name,
+    );
+  });
 }
 bootstrap();
