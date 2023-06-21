573344319272c1ae1934f7e87b6686783c31e93b
enoc de jesus soto arenilla <tecenocdejesus@gmail.com>
2023-06-21 16:47:45 -0500

    se modifica package.json y se mueve migration folder, se debe de corregir las migraciones.

--- src/config/ormconfig-migrations.ts
+++ src/config/ormconfig-migrations.ts
@@ -9,18 +9,18 @@ import { readdirSync } from 'fs';
 
 export const configDatasource: DataSourceOptions & SeederOptions = {
   type: 'mysql',
-  host: process.env.DB_HOST || 'db',
-  port: parseInt(process.env.DB_PORT),
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT) || 3307,
   username: process.env.DB_USER || 'fran',
   password: process.env.DB_PASSWORD || 'passWord',
   database: process.env.DB_DATABASE || 'agenda',
   entities: [`${__dirname}/../**/**/*.entity{.ts,.js}`],
-  migrations: readdirSync(path.join(__dirname, '../migration')).map(
-    (item) => `${path.join(__dirname, `../migration/${item}`)}`,
+  migrations: readdirSync(path.join(__dirname, '../../migration')).map(
+    (item) => `${path.join(__dirname, `../../migration/${item}`)}`,
   ),
   migrationsTableName: 'migrations',
   logging: false,
-  synchronize: true,
+  synchronize: false,
   seeds: [MainSeeder],
 };
 
